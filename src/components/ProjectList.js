acff26fcf0611127a4a3dfbe249904ee7841e15f
Victor Lam <mingmanhk@gmail.com>
2021-11-23 01:55:10 -0800

    updated project info

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -8,14 +8,53 @@ export default function List({ projects }) {
           <h2>Portfolio</h2>
         </div>
         <div class="row">
-           {projects.map((project) => (
+          {projects.map((project) => (
             <div class="col-12 col-md-5">
-             <div class="portfolio-wrap">
-                <h4 class="title"><a target="_blank" rel="noopener noreferrer" href={`${project.project}`}>{project.name}</a></h4> 
-              <a target="_blank" rel="noopener noreferrer" href={`${project.deployment}`}><img src={`${project.img}`} class="img-fluid" alt={`${project.alt}`}></img></a>
+              <div class="portfolio-wrap">
+                <h4 class="title">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={`${project.project}`}
+                  >
+                    {project.name}
+                  </a>
+                </h4>
+                <h6>
+                  Github link:{" "}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={`${project.project}`}
+                  >
+                    {project.project}
+                  </a>
+                </h6>
+                <h6>
+                  Deployment link:{" "}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={`${project.deployment}`}
+                  >
+                    {project.deployment}
+                  </a>
+                </h6>
+                <h6>Summary: {project.summary}</h6>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={`${project.deployment}`}
+                >
+                  <img
+                    src={`${project.img}`}
+                    class="img-fluid"
+                    alt={`${project.alt}`}
+                  ></img>
+                </a>
+              </div>
             </div>
-             </div>
-              ))}
+          ))}
         </div>
       </div>
     </section>
--- src/components/projects.js
+++ src/components/projects.js
@@ -5,6 +5,8 @@ const projects = [
     deployment: "https://habitron-ivs.herokuapp.com/",
     img: "https://github.com/xaker00/habitron/raw/master/image/Home.jpg",
     alt: "Weather-Dashboard",
+    summary:
+      "A Habit Tracker keep track of your habits. Built using Materialize, Chart.js, MySQL2, Express, Sequelize, bcrypt, Session, Handlebars and dotenv.",
   },
   {
     name: "Tech Blog",
@@ -12,6 +14,8 @@ const projects = [
     deployment: "https://victor-tech-blog.herokuapp.com/",
     img: "https://github.com/mingmanhk/tech-blog/raw/main/src/demo.gif",
     alt: "tech-blog",
+    summary:
+      "A simple tech blog using Model View Controller (MVC) paradigm. Built using MySQL2, Express, Sequelize, bcrypt, Session, Handlebars, Bootstrap and dotenv.",
   },
   {
     name: "Employee Tracker",
@@ -19,6 +23,8 @@ const projects = [
     deployment: "https://github.com/mingmanhk/employee-tracker",
     img: "https://github.com/mingmanhk/employee-tracker/raw/main/src/demo.jpg",
     alt: "employee-tracker",
+    summary:
+      "Employee Tracker is a Node.js command-line application using inquirer, chalk, console.table, figlet and mysql to manage employee database.",
   },
   {
     name: "E-Commerce Back End",
@@ -26,6 +32,8 @@ const projects = [
     deployment: "https://github.com/mingmanhk/e-commerce-back-end",
     img: "https://github.com/mingmanhk/e-commerce-back-end/raw/main/src/Categories-Demo.gif",
     alt: "e-commerce-back-end",
+    summary:
+      "Create a back end for an e-commerce site. This program is using Express.js API to use Sequelize to interact with a MySQL database.",
   },
   {
     name: "Find-My-Plate",
@@ -33,6 +41,8 @@ const projects = [
     deployment: "https://troyat88.github.io/Find-My-Plate/",
     img: "https://github.com/troyat88/Find-My-Plate/raw/main/assets/images/Screenshot.png",
     alt: "Find-My-Plate",
+    summary:
+      "An easy to use application that allows users to find dining options in their current area or zip-code, search by cuisine, and search menus.",
   },
   {
     name: "Weather Dashboard",
@@ -40,6 +50,8 @@ const projects = [
     deployment: "https://mingmanhk.github.io/Weather-Dashboard/",
     img: "https://github.com/mingmanhk/Weather-Dashboard/raw/main/Assets/06-server-side-apis-homework-demo.png",
     alt: "Weather-Dashboard",
+    summary:
+      "Use the OpenWeather One Call API to retrieve weather data for cities.",
   },
   {
     name: "Work-Day-Scheduler",
@@ -47,6 +59,8 @@ const projects = [
     deployment: "https://mingmanhk.github.io/Work-Day-Scheduler/",
     img: "https://github.com/mingmanhk/Work-Day-Scheduler/raw/main/assets/05-third-party-apis-homework-demo.gif",
     alt: "Work-Day-Scheduler",
+    summary:
+      "Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.",
   },
   {
     name: "Password Generator",
@@ -54,6 +68,8 @@ const projects = [
     deployment: "https://mingmanhk.github.io/Password-Generator/",
     img: "https://github.com/mingmanhk/Password-Generator/raw/main/src/Password%20Generator.jpg",
     alt: "Password-Generator",
+    summary:
+      "An application that enables employees to generate random passwords based on criteria",
   },
 ];
 
