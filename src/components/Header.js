ab444277fd582f9e0743cf15d67ec754fe3cf1c4
mingmanhk@gmail.com <mingmanhk@gmail.com>
2025-11-07 13:17:06 -0800

    minor bug fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Profile from "./Profile";
 
 // In Navbar, we can assign a style from an object by using curly braces
 function Header() {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+    document.body.classList.toggle('mobile-nav-active');
+  };
+
   return (
-    <header>
-      <div className="d-flex flex-column">
-        <Profile />
-        <Navbar />
-      </div>
-    </header>
+    <>
+      <i
+        className={`bi mobile-nav-toggle ${isMobileNavOpen ? 'bi-x' : 'bi-list'}`}
+        onClick={toggleMobileNav}
+      ></i>
+      <header id="header">
+        <div className="d-flex flex-column">
+          <Profile />
+          <Navbar />
+        </div>
+      </header>
+    </>
   );
 }
 
