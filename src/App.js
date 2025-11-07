80e6624233f77cd7077df73bae00b629d1cfe5e9
mingmanhk@gmail.com <mingmanhk@gmail.com>
2025-11-07 12:56:59 -0800

    minor update npm deprecated code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Header from "./components/Header.js";
 import About from "./components/about.js";
 import Intro from "./intro.js";
