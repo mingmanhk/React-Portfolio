7794392c9343d1dd752c997ebb136c284a5e4ff9
mingmanhk@gmail.com <mingmanhk@gmail.com>
2025-11-07 12:41:44 -0800

    Add Adsense code

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import Navbar from "./Navbar";
 import Profile from "./Profile";
 
 // In Navbar, we can assign a style from an object by using curly braces
-function Header({ theme, toggleTheme }) {
+function Header() {
   return (
     <header>
       <div className="d-flex flex-column">
-        <Profile theme={theme} toggleTheme={toggleTheme} />
+        <Profile />
         <Navbar />
       </div>
     </header>
