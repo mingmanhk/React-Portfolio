f21c35e2bca72f0325d039d49d6c97495800ef79
mingmanhk <mingmanhk@gmail.com>
2025-12-03 07:08:19 +0000

    docs: updated vite config to fix github pages deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/React-Portfolio/",
   server: {
     port: 3000,
     open: true
