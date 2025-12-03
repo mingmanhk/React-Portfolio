ff56cc380a66e3c7253dc2c5692e211534a3584d
mingmanhk <mingmanhk@gmail.com>
2025-12-03 07:18:03 +0000

    Fix resume link and redeploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Portfolio/",
+  base: "/",
   server: {
     port: 3000,
     open: true
