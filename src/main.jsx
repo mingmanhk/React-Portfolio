b7395c5f93ee007bc872dc05e9dbfe3496b7a11f
mingmanhk <mingmanhk@gmail.com>
2025-12-06 09:32:19 +0000

    feat: add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -10,5 +11,6 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
